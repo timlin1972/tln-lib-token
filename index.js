0e52d523db51c330ba44c59c81846074b004637c
timlin <timlin1972@gmail.com>
2020-11-30 23:44:31 +0800

    Add i18n

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const MODULE_NAME = 'lib-token';
 const DEF_LOGGER = null;
 const DEF_SECRET = 'Tim';
 const DEF_EXPIRES_IN = '1d';
+const DEF_I18N = null;
 
 const DEF_CONFIGS = {
   logger: DEF_LOGGER,
   secret: DEF_SECRET,
   expiresIn: DEF_EXPIRES_IN,
+  i18n: DEF_I18N,
 }
 
 class LibToken {
@@ -17,8 +19,15 @@ class LibToken {
     this.logger = configs.logger || DEF_LOGGER;
     this.secret = configs.secret || DEF_SECRET;
     this.expiresIn = configs.expiresIn || DEF_EXPIRES_IN;
+    this.i18n = configs.i18n || DEF_I18N;
 
-    this.log('info', 'Initialized');
+    if (!this.i18n) {
+      this.log('error', 'I18n must be provided.');
+      reject(new Error('I18n must be provided.'))
+      return;
+    }
+
+    this.log('info', this.i18n.t('inited'));
   }
 
   userToToken = (user) => jwt.sign(user, this.secret, { expiresIn: this.expiresIn });
